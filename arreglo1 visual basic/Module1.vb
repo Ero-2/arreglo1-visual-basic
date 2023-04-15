db0ca734bd771168e5475a872fd3ff1939c764cd
erick garcia <erick garcia@DESKTOP-QFHIISE>
2023-04-15 17:03:06 -0600

    Agregar archivos de proyecto.

---
+++ arreglo1 visual basic/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim valores As Integer() = New Integer() {2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
+        Dim suma As Integer = 0
+
+        For i As Integer = 0 To valores.Length - 1
+            suma += valores(i)
+        Next
+
+        Console.WriteLine("La suma de los valores del arreglo es: {0}", suma)
+        Console.ReadKey()
+    End Sub
+
+End Module
